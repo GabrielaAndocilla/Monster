d5cb0c9873f5f99a20770322d8d94a5b9595973f
Gabriela Andocilla <gabrielaandocilla@hotmail.com>
2020-07-17 01:19:51 -0500

    Web

---
+++ Assets/Scenes/ColiderCoin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColiderCoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other is BoxCollider)
+        {
+            other.GetComponent<Player>().points = other.GetComponent<Player>().points+1;
+            Destroy(gameObject);
+            print("Se destruye");
+        }
+    }
+}
